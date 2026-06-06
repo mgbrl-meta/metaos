47db34aa12bff8eb29696b25c0e8872c025bb9a5
Maulik Gupta <Maulik@Brillares-MacBook-Air.local>
2026-06-06 13:59:34 +0530

    Fix ReactNode import in priority split tabs

--- components/meta/PrioritySplitTabs.tsx
+++ components/meta/PrioritySplitTabs.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import { ChevronDown, ShieldAlert, TrendingUp } from "lucide-react";
 import {
   CartesianGrid,
